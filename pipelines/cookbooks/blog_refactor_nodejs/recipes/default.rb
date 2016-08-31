ac9dcc5f34e36ba2cf39c01bb8d5129a10389873
Jeffrey Dugas <jedugas@gmail.com>
2016-08-30 22:28:44 -0400

    Install cfn-init on ASG instances; support cfn-signal

--- pipelines/cookbooks/blog_refactor_nodejs/recipes/default.rb
+++ pipelines/cookbooks/blog_refactor_nodejs/recipes/default.rb
@@ -2,6 +2,19 @@ include_recipe 'apt'
 include_recipe 'nodejs'
 include_recipe 'nodejs::npm'
 
+script 'install cfn-init' do
+  interpreter "bash"
+  code <<-EOH
+    apt-get -y install python-setuptools
+    wget -P /root https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+    mkdir -p /root/aws-cfn-bootstrap-latest
+    tar xvfz /root/aws-cfn-bootstrap-latest.tar.gz --strip-components=1 -C /root/aws-cfn-bootstrap-latest
+    easy_install /root/aws-cfn-bootstrap-latest/
+  EOH
+end
+execute 'cfn-init' do
+end
+
 # global npm installations
 %w{jslint forever forever-monitor}.each do |pkg|
   nodejs_npm pkg
@@ -10,7 +23,7 @@ end
 # local npm installations
 %w{config mysql}.each do |pkg|
   nodejs_npm pkg do
-    path "#{node[:blog_refactor_nodejs][:folder]}"
+    path "#{node[:blog_refactor_nodejs][:folder]}/app"
   end
 end
 
@@ -19,6 +32,6 @@ template "#{node[:blog_refactor_nodejs][:folder]}/app/config/default.json" do
   mode '755'
 end
 
-execute "application startup" do
+execute 'application startup' do
   command "forever start --sourceDir #{node[:blog_refactor_nodejs][:folder]} index.js"
 end
--- pipelines/deploy.sh
+++ pipelines/deploy.sh
@@ -5,11 +5,11 @@ export AWS_REGION="us-east-1"
 export AWS_VPC_ID="vpc-857a3ee2"
 export AWS_SUBNET_IDS="subnet-c5a76a8c,subnet-3b233a06"
 export AWS_AZS="us-east-1c,us-east-1b"
-export AWS_KEYPAIR="example.pem"
+export AWS_KEYPAIR="dugas-labs"
 
 # set up local variables
-app_name=blog_refactor_nodejs
-repository_url=https://github.com/stelligent/${app_name}
+app_name=NodeJSApp
+repository_url=https://github.com/stelligent/blog_refactor_nodejs
 repository_branch=master
 aws_region=${AWS_REGION:-us-east-1}
 aws_vpc=${AWS_VPC_ID}
@@ -23,7 +23,7 @@ git clone --branch ${repository_branch} --depth 1 ${repository_url} .working-fol
 
 # perform static analysis on the code
 pushd ./.working-folder
-  foodcritic -t ~FC001 pipelines/cookbooks/${app_name}
+  foodcritic -t ~FC001 ./pipelines/cookbooks/blog_refactor_nodejs
   find . -name "*.js" -print0 | xargs -0 jslint
 popd
 
@@ -32,7 +32,7 @@ stamp=$(date +%Y%m%d%H%M%s)
 
 # run aws cli for cloudformation of ASG
 asg_stack_name="${app_name}-${stamp}"
-cfn_template=${DEPLOY_TEMPLATE:./cfn/deploy-app.template}
+cfn_template=${DEPLOY_TEMPLATE:-./cfn/deploy-app.template}
 aws cloudformation create-stack \
   --disable-rollback \
   --region ${aws_region} \
@@ -51,11 +51,12 @@ aws cloudformation create-stack \
     ParameterKey=PropertyStr,ParameterValue=${PropertyStr:-banjo} \
     ParameterKey=PropertyNum,ParameterValue=${PropertyNum:-144} \
     ParameterKey=PropertyBool,ParameterValue=${PropertyBool:-true} \
-    ParameterKey=PropertyUrl,ParameterValue=${PropertyUrl:-https://jqplay.org/} \
+    ParameterKey=PropertyUrl,ParameterValue=${PropertyUrl:-https://jqplay.org/}
     
 aws cloudformation wait stack-create-complete --stack-name ${asg_stack_name}
+echo $(aws cloudformation describe-stacks --stack-name ${rds_stack_name} 2>/dev/null) > .working-folder/app.tmp
 
-elb_dns=$(cat rds.tmp | jq '.Stacks[0].Outputs[] | select(.OutputKey == "DNSName") | .OutputValue')
+elb_dns=$(cat .working-folder/app.tmp | jq '.Stacks[0].Outputs[] | select(.OutputKey == "DNSName") | .OutputValue')
 elb_url="https://%{elb_dns}/:8080"
 
 # post-deploy smoke test

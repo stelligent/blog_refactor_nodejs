edf517ccaafd3876881179b5ffb514ec6b498833
Jeffrey Dugas <jeff.dugas@stelligent.com>
2016-08-31 09:00:35 -0400

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 'use strict';
 var http = require('http');
 var config = require('config');
-var dbConfig = config.get('app.config');
+var appConfig = config.get('app.config');
 var server = http.createServer(function (request, response) {
     response.writeHead(200);
-    response.write('<p>' + JSON.stringify(dbConfig) + '</p>');
+    response.write('<p>' + JSON.stringify(appConfig) + '</p>');
 	response.end('<p>HTTP' + request.method + " @ " + Date.now() + '</p>');
 });
 server.listen(8080);
